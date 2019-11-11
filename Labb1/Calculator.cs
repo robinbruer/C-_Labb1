37c05172358e6f6023ce50d5b99a24b53c355bec
robinbruer <r_bruer@hotmail.com>
2019-11-11 16:33:28 +0100

    Initial check-in of module Labb1

---
+++ Labb1/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Labb1
+{
+    public class Calculator
+    {
+        public double FirstNumber { get; set; }
+        public double SecondNumber { get; set; }
+        public double NumberInMemory { get; set; }
+
+        public Calculator()
+        {
+            FirstNumber = 0;
+            SecondNumber = 0;
+            NumberInMemory = 0; 
+            
+        }
+
+        public override string ToString()
+        {
+            return $"First number: { FirstNumber } Second number: { SecondNumber } Number in memory: { NumberInMemory }";
+        }
+
+        public double SumNumbers () => FirstNumber + SecondNumber;
+        
+        public double MultiplyNumbers () => FirstNumber * SecondNumber;
+
+        public double SumInBetween ()
+        {
+            double sum = 0;
+
+            for (double i = FirstNumber; i <= SecondNumber; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Labb1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Labb1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+            while (true)
+            {
+                Console.WriteLine("1.Add two numbers\n" +
+                    "2.Multiply two numbers\n" +
+                    "3.Sum the numbers between two numbers.\n" +
+                    "4.Add number to memory\n" +
+                    "5.Clear memory\n" +
+                    "6.Print calculator contents.\n" +
+                    "7.Quit");
+
+                int readInput = int.Parse(Console.ReadLine());
+
+                switch (readInput)
+                {
+                    case 1:
+                        SetNumbers();
+                        Console.WriteLine($"Result: { calculator.SumNumbers() } \n");
+                        break;
+                    case 2:
+                        SetNumbers();
+                        Console.WriteLine($"Result: { calculator.MultiplyNumbers() } \n");
+                        break;
+                    case 3:
+                        SetNumbers();
+                        Console.WriteLine($"Result: { calculator.SumInBetween() } \n");
+                        break;
+                    case 4:
+                        Console.WriteLine("Add number to memory ");
+                        calculator.NumberInMemory = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Number successfully added to memory\n");
+                        break;
+                    case 5:
+                        calculator = new Calculator();
+                        Console.WriteLine("Number in memory cleared\n");
+                        break;
+                    case 6:
+                        Console.WriteLine(calculator + "\n");
+                        break;
+                    case 7:
+                        Console.WriteLine("Goodbye");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            void SetNumbers()
+            {
+                Console.WriteLine("Insert first number to add: ");
+                calculator.FirstNumber = double.Parse(Console.ReadLine());
+                Console.WriteLine("Insert second number to add: ");
+                calculator.SecondNumber = double.Parse(Console.ReadLine());
+            }
+        }
+
+        
+    }
+}
